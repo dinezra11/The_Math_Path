e78b0fe37ac43fb8f73c224d8a0acbb84e37dbbc
deanMansour <damannov15@gmail.com>
2021-12-25 20:52:12 +0200

    about us

--- scene_about_us.py
+++ scene_about_us.py
@@ -7,17 +7,29 @@ class Aboutus(Scene):
     # ***********************************************************************************************************
     def __init__(self, display):
         self.white = (255, 255, 255)
+        self.x = 1024
         self.width = 1024
         self.hight = 800
         self.display_surface = pygame.display.set_mode((self.width, self.hight))
         self.image = pygame.image.load("images/about-us.png")
+        # init an exit button
+        self.blue = (0, 0, 128)
+        self.grey = (127, 127, 127)
+        self.font = pygame.font.Font('fonts/defaultFont.ttf', 32)
+        self.text = self.font.render(" Exit ", True, self.blue, self.grey)
+        self.textRect = self.text.get_rect()
+        self.textRect.center = (self.x - 45, 20)
 
     # ***********************************************************************************************************
     def update(self):
+        mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
-
+            # if clicked on exit------------------------
+            elif self.textRect.collidepoint(mouse_pos):
+                if pygame.mouse.get_pressed()[0]:
+                    return False  # Return userId so the system will go back to the user's menu screen
         return True
 
     # ***********************************************************************************************************
@@ -27,12 +39,7 @@ class Aboutus(Scene):
         # to the display surface object at
         # (0, 0) coordinate.
         self.display_surface.blit(self.image, (0, 0))
-       # pygame.display.update()
-
+        # display exit button
+        self.display_surface.blit(self.text, self.textRect)
+        # pygame.display.update()
         pass
-
-
-
-
-
-
